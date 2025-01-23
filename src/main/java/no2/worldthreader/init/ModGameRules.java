62176a27f38e52c0a519dded7afc65b945434529
2No2Name <2no2name@web.de>
2025-01-23 01:02:10 +0100

    Disable additional tick after teleport by default

--- src/main/java/no2/worldthreader/init/ModGameRules.java
+++ src/main/java/no2/worldthreader/init/ModGameRules.java
@@ -11,13 +11,13 @@ public class ModGameRules {
 	public static final boolean INITIAL_TRUE = true;
 	public static final boolean INITIAL_FALSE = false;
 	public static BoolRule TELEPORTED_ENTITY_ADDITIONAL_TICK;
-	public static boolean SHOULD_TICK_ENTITY_AFTER_TELEPORT = INITIAL_TRUE;
+	public static boolean SHOULD_TICK_ENTITY_AFTER_TELEPORT = INITIAL_FALSE;
 
 	public static void registerGameRules() {
 		try {
 			ACTIVE = BoolRule.builder("Active", GameRules.Category.MISC).setInitial(INITIAL_TRUE)
 					.setCallback((server, value) -> ((MinecraftServerExtended) server).worldthreader$setThreadingEnabled(value.get())).build();
-			TELEPORTED_ENTITY_ADDITIONAL_TICK = BoolRule.builder("AdditionalEntityTickAfterTeleport", GameRules.Category.MISC).setInitial(INITIAL_TRUE)
+			TELEPORTED_ENTITY_ADDITIONAL_TICK = BoolRule.builder("AdditionalEntityTickAfterTeleport", GameRules.Category.MISC).setInitial(INITIAL_FALSE)
 					.setCallback((server, value) -> server.execute(() -> SHOULD_TICK_ENTITY_AFTER_TELEPORT = value.get())).build();
 		} catch (Throwable exception) {
 			WorldThreaderMod.LOGGER.error("Worldthreader: Could not register gamerules. Using default values!");
