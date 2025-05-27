e83ec075d42e5bb97a764aa20cf71b276cf6a84d
2No2Name <2no2name@web.de>
2025-05-27 14:27:26 +0200

    Fix falling block duplication not working
    https://github.com/2No2Name/worldthreader/issues/23

--- src/main/java/no2/worldthreader/mixin/dimension_change/departure/FallingBlockEntityMixin.java
+++ src/main/java/no2/worldthreader/mixin/dimension_change/departure/FallingBlockEntityMixin.java
@@ -1,21 +1,36 @@
 package no2.worldthreader.mixin.dimension_change.departure;
 
-import net.minecraft.resources.ResourceKey;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.level.Level;
-import no2.worldthreader.common.mixin_support.interfaces.EntityExtended;
+import net.minecraft.world.level.portal.TeleportTransition;
+import no2.worldthreader.common.mixin_support.interfaces.MinecraftServerExtended;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FallingBlockEntity.class)
-public abstract class FallingBlockEntityMixin implements EntityExtended {
+public abstract class FallingBlockEntityMixin extends Entity {
 
     @Shadow public boolean forceTickAfterTeleportToDuplicate;
 
-    @Override
-    public void worldthreader$onEntityDepartsFromServerWorld(ResourceKey<Level> targetDimension, ResourceKey<Level> sourceDimension) {
-        //noinspection UnnecessaryLocalVariable
-        boolean entersOrLeavesEnd = (sourceDimension == Level.END || targetDimension == Level.END) && sourceDimension != targetDimension;
-        this.forceTickAfterTeleportToDuplicate = entersOrLeavesEnd;
+    public FallingBlockEntityMixin(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(
+            method = "teleport(Lnet/minecraft/world/level/portal/TeleportTransition;)Lnet/minecraft/world/entity/Entity;",
+            at = @At(value = "RETURN")
+    )
+    public void worldthreader$allowFallingBlockDuplication(TeleportTransition teleportTransition, CallbackInfoReturnable<Entity> cir, @Local boolean changedFromOrToEnd, @Local Entity newEntity) {
+        if (changedFromOrToEnd && newEntity == null && this.level() instanceof ServerLevel serverLevel
+                && ((MinecraftServerExtended) serverLevel.getServer()).worldthreader$isTickMultithreaded()) {
+            this.forceTickAfterTeleportToDuplicate = true;
+        }
     }
 }
