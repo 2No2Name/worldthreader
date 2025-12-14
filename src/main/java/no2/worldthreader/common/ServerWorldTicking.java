5ddcec6639e5e0a6aa6d37a244e987bbb8e8f0aa
2No2Name <2no2name@web.de>
2025-12-14 12:10:59 +0100

    Fix barrier not waking up waiting thread correctly
    (cherry picked from commit a957fdf05cc8b43add105cdfb6e3b662238a7bc0)

--- src/main/java/no2/worldthreader/common/ServerWorldTicking.java
+++ src/main/java/no2/worldthreader/common/ServerWorldTicking.java
@@ -91,7 +91,7 @@ public class ServerWorldTicking {
     private static void delegateCrash(Throwable throwable, String title, ServerLevel serverLevel, WorldThreadingManager worldThreadingManager) {
         String serverLevelOwner = ((ThreadOwnedObject) serverLevel).worldthreader$getOwningThread().getName();
         String chunkCacheOwner = ((ThreadOwnedObject) serverLevel.getChunkSource()).worldthreader$getOwningThread().getName();
-        worldThreadingManager.tryGiveAwayExclusiveWorldAccess(); //If the exception was thrown while this thread held exclusive access, it must be returned.
+        worldThreadingManager.tryGiveAwayExclusiveWorldAccess();
 
         CrashReport crashReport = CrashReport.forThrowable(throwable, title);
         serverLevel.fillReportDetails(crashReport);
